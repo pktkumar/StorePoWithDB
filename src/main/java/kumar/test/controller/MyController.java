555cc9d824655ecba9543efb15278715f55410ce
Vaduganathan <KUMAR_VADUGANATHAN@homedepot.com>
2019-01-02 12:47:09 -0500

    upodated with storePO

--- src/main/java/kumar/test/controller/MyController.java
+++ src/main/java/kumar/test/controller/MyController.java
@@ -2,14 +2,12 @@ package kumar.test.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import kumar.test.DAO.ConnetDB;
 import kumar.test.DAO.ReturnValues;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,28 +17,41 @@ public class MyController {
 	@Autowired
 	ConnetDB connetDB;
 	
-	@RequestMapping("/")
-	public String testDB(@RequestParam int skuNbr ){
-		//http://localhost:8080/?skuNbr=106270
-		//int skuNbr=106270;
+	
+	@RequestMapping(value="/", method= {RequestMethod.GET,RequestMethod.POST},produces={MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	//@RequestMapping("/")
+	public List<ReturnValues> testDB(@RequestParam int skuNbr ){
+	
+	
+		
 		String returnVal="";
 		List<ReturnValues> returnValues=new ArrayList<ReturnValues>();
+		returnValues = connetDB.getStorePOs(skuNbr);
+		        // just added see in the console
+				for(ReturnValues item : returnValues){
+					returnVal=item.getPo_ctrl_nbr();
+					System.out.println(returnVal);
+				}
 		
-		 returnValues = connetDB.getStorePOs(skuNbr);
-		
-			for(ReturnValues item : returnValues){
-				System.out.println(item.getPo_ctrl_nbr());
-				
-				returnVal=item.getPo_ctrl_nbr();
-			}
-			System.out.println("..........");
-		
-		return returnVal;
-		
+		return returnValues;
+			
 	}
+	
+	
 	
 
 }
+
+//http://www.springboottutorial.com/spring-boot-content-negotiation-with-xml-json-representations
+
+
+/*
+  in browser it will come only with xml reponse(in postman will support both xml and json resonce)
+  http://localhost:8080/?skuNbr=106270
+  
+ 
+  
+*/
 
 
 /*to add first time
